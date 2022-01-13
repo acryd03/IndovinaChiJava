bb5cd7283d8c6d255df7627d308a1cfe3148bb3a
GioCass03 <giorgiocassina2003@gmail.com>
2022-01-13 19:46:44 +0100

    IndovinaChi 1.6
    Risolto problema bottone pronto

--- IndovinaChi/src/indovinachi/Condivisa.java
+++ IndovinaChi/src/indovinachi/Condivisa.java
@@ -26,7 +26,7 @@ public class Condivisa {
     boolean prontoDest=false;
 
     private Condivisa() throws SocketException, UnknownHostException {
-        socket = new UtilPacchetto(12345, "localhost"); //socket per ascolto
+        socket = new UtilPacchetto(12347, "localhost"); //socket per ascolto
         destinatario = "";
         nome = "";
         messaggio = "";
